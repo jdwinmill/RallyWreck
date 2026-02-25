3a787fa0e7f55014d953379a7b9c7fa34c02947f
jdwinmill <jakewinmill@me.com>
2026-02-24 17:24:22 -0700

    Initial Commit

---
+++ Rallywreck/RallywreckApp.swift
@@ -0,0 +1,17 @@
+//
+//  RallywreckApp.swift
+//  Rallywreck
+//
+//  Created by Jacob Winmill on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct RallywreckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RallywreckTests/RallywreckTests.swift
@@ -0,0 +1,17 @@
+//
+//  RallywreckTests.swift
+//  RallywreckTests
+//
+//  Created by Jacob Winmill on 2/24/26.
+//
+
+import Testing
+@testable import Rallywreck
+
+struct RallywreckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
